cdbfceb296d4d0d02845ef4c2aca98865f7c67ea
Mczgodpiggy <63181866+Mczgodpiggy@users.noreply.github.com>
2023-10-21 16:25:45 +0800

    Forgot something

--- Sapphire LITE/Properties/AssemblyInfo.cs
+++ Sapphire LITE/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sapphire LITE Clicker")]
 [assembly: AssemblyDescription("Recoded by Mczgodpiggy@0388")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Starlight Technology, LLC")]
-[assembly: AssemblyProduct("Sapphire AC")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sapphire Reborn")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
